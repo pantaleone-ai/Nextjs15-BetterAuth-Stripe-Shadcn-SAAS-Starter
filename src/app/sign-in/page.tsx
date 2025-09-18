7eae525ee43e82ff63cef901eeb4acd967c601ec
pantaleone-ai <mattpantaleone@gmail.com>
2025-09-17 20:28:51 -0600

    &apos;

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -81,7 +81,7 @@ export default function SignInPage() {
           <SocialLogin />
 
           <div className="text-center text-sm">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Link href="/sign-up" className="text-primary hover:underline">
               Sign up
             </Link>
