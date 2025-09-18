9067268a0a3520419735ed3780c9cfcfb7c382e4
pantaleone-ai <mattpantaleone@gmail.com>
2025-09-18 13:08:50 -0600

    fix label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import { cn } from '@/lib/utils'
+"use client"
 
-export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
+import { cva, type VariantProps } from "class-variance-authority"
 
-const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <label
-        ref={ref}
-        className={cn(
-          "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-          className
-        )}
-        {...props}
-      />
-    )
-  }
+import { cn } from "@/lib/utils"
+
+const labelVariants = cva(
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
-Label.displayName = "Label"
+// Change from interface to type alias
+type LabelProps = React.ComponentProps<typeof LabelPrimitive.Root> &
+  VariantProps<typeof labelVariants>
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  LabelProps
+>(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(labelVariants(), className)}
+    {...props}
+  />
+))
+Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
